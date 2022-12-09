661233c13cb7225d982a10b3dc9155ed0c1b7ab5
wasilewskry <93921489+wasilewskry@users.noreply.github.com>
2022-12-09 09:01:20 +0100

    init

---
+++ run.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from config import token
+
+
+class Sphynx(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned,
+                         intents=discord.Intents.all())
+
+        self.initial_extensions = [
+            'cogs.admin',
+        ]
+
+    async def setup_hook(self):
+        for ext in self.initial_extensions:
+            await self.load_extension(ext)
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+if __name__ == '__main__':
+    bingus = Sphynx()
+    bingus.run(token)
