8fd469f68434849d96eab5fe555cbb2245fb0dfd
wasilewskry <93921489+wasilewskry@users.noreply.github.com>
2022-12-26 00:03:54 +0100

    Fix duplicated slash commands when syncing

--- cogs/owner.py
+++ cogs/owner.py
@@ -6,21 +6,19 @@ class Owner(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.is_owner()
+    async def globalsync(self, ctx: commands.Context):
+        """Syncs slash commands globally"""
+        synced = await ctx.bot.tree.sync()
+        await ctx.send(f"Synced {len(synced)} commands globally.")
+
+    @commands.command()
     @commands.guild_only()
     @commands.is_owner()
     async def localsync(self, ctx: commands.Context):
-        """Syncs slash commands to current guild. Can only be used in a guild."""
-        self.bot.tree.copy_global_to(guild=ctx.guild)
+        """Syncs slash commands to current guild"""
         synced = await ctx.bot.tree.sync(guild=ctx.guild)
         await ctx.send(f"Synced {len(synced)} commands to this guild.")
-
-    @commands.command()
-    @commands.dm_only()
-    @commands.is_owner()
-    async def globalsync(self, ctx: commands.Context):
-        """Syncs slash commands globally. Can only be used in DMs."""
-        synced = await ctx.bot.tree.sync()
-        await ctx.send(f"Synced {len(synced)} commands globally.")
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
