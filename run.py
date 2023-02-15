17b3cc5f93a7374ce6a5b38b2c3c2645d0743965
wasilewskry <93921489+wasilewskry@users.noreply.github.com>
2023-02-15 16:48:14 +0100

    Add methods that check the cache first before fetching

--- run.py
+++ run.py
@@ -29,6 +29,18 @@ class Sphynx(commands.Bot):
         for ext in self.initial_extensions:
             await self.load_extension(ext)
 
+    async def maybe_fetch_user(self, user_id: int) -> discord.User:
+        if user := self.get_user(user_id):
+            return user
+        else:
+            return await self.fetch_user(user_id)
+
+    async def maybe_fetch_channel(self, channel_id: int):
+        if user := self.get_channel(channel_id):
+            return user
+        else:
+            return await self.fetch_channel(channel_id)
+
 
 # -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
