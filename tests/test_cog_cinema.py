8cf86cfe5809f690b4833595b1e626d2359c55f2
wasilewskry <93921489+wasilewskry@users.noreply.github.com>
2023-04-10 21:55:47 +0200

    Add tests for utils

--- tests/test_cog_cinema.py
+++ tests/test_cog_cinema.py
@@ -1,4 +1,3 @@
-import pytest
 from discord.app_commands import Choice
 
 from cogs.cinema.helpers import deduplicate_autocomplete_labels, prepare_production_autocomplete_choices
@@ -6,59 +5,44 @@ from cogs.cinema.models import Production
 
 
 class TestHelpers:
-    @pytest.fixture
-    def choices(self):
-        return [
+    def test_deduplicate_autocomplete_labels(self):
+        choices = [
+            Choice(name='Taylor John Smith', value=1369328),
             Choice(name='Taylor John Smith', value=1369329),
-            Choice(name='Michael St. John Smith', value=77039),
             Choice(name='John Smith', value=151004),
             Choice(name='Timothy John Smith', value=1419122),
             Choice(name='John N. Smith', value=60295),
             Choice(name='John Smith', value=1291629),
             Choice(name='John Smith Kabashi', value=2374831),
-            Choice(name='John Victor-Smith', value=29538),
             Choice(name='John Smith', value=6118),
             Choice(name='John Smithson', value=68542),
-            Choice(name='John Smiley', value=1501158),
             Choice(name='John Smith', value=2607051),
-            Choice(name='John A. Small', value=2725385),
-            Choice(name='John Smallcombe', value=1486280),
-            Choice(name='John Smythe', value=1505864),
-            Choice(name='John Temple-Smith', value=38062),
             Choice(name='John T. Smith', value=930110),
             Choice(name='N. John Smith', value=72083),
             Choice(name='John Smith', value=1853052),
             Choice(name='John Smith', value=2603116),
         ]
-
-    @pytest.fixture
-    def deduplicated_choices(self):
-        return [
-            Choice(name='Taylor John Smith', value=1369329),
-            Choice(name='Michael St. John Smith', value=77039),
+        expected = [
+            Choice(name='Taylor John Smith (1)', value=1369328),
+            Choice(name='Taylor John Smith (2)', value=1369329),
             Choice(name='John Smith (1)', value=151004),
             Choice(name='Timothy John Smith', value=1419122),
             Choice(name='John N. Smith', value=60295),
             Choice(name='John Smith (2)', value=1291629),
             Choice(name='John Smith Kabashi', value=2374831),
-            Choice(name='John Victor-Smith', value=29538),
             Choice(name='John Smith (3)', value=6118),
             Choice(name='John Smithson', value=68542),
-            Choice(name='John Smiley', value=1501158),
             Choice(name='John Smith (4)', value=2607051),
-            Choice(name='John A. Small', value=2725385),
-            Choice(name='John Smallcombe', value=1486280),
-            Choice(name='John Smythe', value=1505864),
-            Choice(name='John Temple-Smith', value=38062),
             Choice(name='John T. Smith', value=930110),
             Choice(name='N. John Smith', value=72083),
             Choice(name='John Smith (5)', value=1853052),
             Choice(name='John Smith (6)', value=2603116),
         ]
 
-    @pytest.fixture
-    def candidates(self):
-        return [
+        assert deduplicate_autocomplete_labels(choices) == expected
+
+    def test_prepare_production_autocomplete_choices(self):
+        candidates = [
             Production(title='The Thing', id=2, popularity=4, release_date='2011-01-01'),
             Production(title='The Thing', id=4, popularity=2),
             Production(title='The Thing', id=1, popularity=5, release_date='1982-01-01'),
@@ -66,10 +50,7 @@ class TestHelpers:
             Production(title='The Things', id=3, popularity=3, release_date='2021-01-01'),
             Production(title='The Thingy', id=6, popularity=0),
         ]
-
-    @pytest.fixture
-    def prepared_candidates(self):
-        return [
+        expected = [
             Choice(name='The Thing (1982)', value=1),
             Choice(name='The Thing (2011)', value=2),
             Choice(name='The Things (2021)', value=3),
@@ -78,8 +59,4 @@ class TestHelpers:
             Choice(name='The Thingy', value=6),
         ]
 
-    def test_deduplicate_autocomplete_labels(self, choices, deduplicated_choices):
-        assert deduplicate_autocomplete_labels(choices) == deduplicated_choices
-
-    def test_prepare_production_autocomplete_choices(self, candidates, prepared_candidates):
-        assert prepare_production_autocomplete_choices(candidates) == prepared_candidates
+        assert prepare_production_autocomplete_choices(candidates) == expected
---
+++ tests/test_utils.py
@@ -0,0 +1,116 @@
+import datetime as dt
+from unittest.mock import Mock
+
+import discord
+import pytest
+from discord import HTTPException
+
+from tests.conftest import MockException
+from utils.misc import trim_by_paragraph, next_datetime, calculate_age, get_timezones, strptime, get_as_json, dm_open
+
+
+class TestTrimByParagraph:
+    def test_short(self):
+        text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        assert trim_by_paragraph(text, fallback_length=100) == text
+
+    def test_long(self):
+        text = (
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+            'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        )
+        expected = (
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+            'sed do eiusmod tempor incididunt ut labo...'
+        )
+        assert trim_by_paragraph(text, fallback_length=100) == expected
+
+    def test_with_paragraphs(self):
+        text = (
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            '\n'
+            'Duis aute irure dolor in reprehenderit.'
+            '\n'
+            'In voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+            'Excepteur sint occaecat cupidatat non proident, '
+            'sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        )
+        expected = (
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            '\n'
+            'Duis aute irure dolor in reprehenderit.'
+        )
+        assert trim_by_paragraph(text, fallback_length=100) == expected
+
+
+class TestNextDatetime:
+    def test_same_day(self):
+        current = dt.datetime(year=2000, month=6, day=6, hour=10, minute=10)
+        expected = dt.datetime(year=2000, month=6, day=6, hour=12, minute=30)
+        assert next_datetime(current, hour=12, minute=30) == expected
+
+    def test_next_day(self):
+        current = dt.datetime(year=2000, month=6, day=6, hour=10, minute=10)
+        expected = dt.datetime(year=2000, month=6, day=7, hour=9, minute=30)
+        assert next_datetime(current, hour=9, minute=30) == expected
+
+
+class TestCalculateAge:
+    born = dt.date(year=2000, month=6, day=6)
+
+    def test_died_early(self):
+        died = dt.datetime(year=2015, month=6, day=5)
+        assert calculate_age(born=self.born, died=died) == 14
+
+    def test_died_late(self):
+        died = dt.datetime(year=2015, month=6, day=7)
+        assert calculate_age(born=self.born, died=died) == 15
+
+
+class TestGetTimezones:
+    # Some systems can return an empty list, we check for that
+
+    def test_get_timezones(self):
+        assert len(get_timezones()) > 0
+
+
+class TestCustomStrptime:
+    date_string = '2000-06-06 12:00'
+
+    def test_with_time(self):
+        assert strptime(self.date_string, '%Y-%m-%d %H:%M') == dt.datetime(year=2000, month=6, day=6, hour=12, minute=0)
+
+    def test_without_time(self):
+        assert strptime(self.date_string, '%Y-%m-%d %H:%M', no_time=True) == dt.date(year=2000, month=6, day=6)
+
+    def test_garbled(self):
+        assert strptime('asdasdsfg', '%Y-%m-%d %H:%M') is None
+
+
+class TestGetAsJson:
+    @pytest.mark.asyncio
+    async def test_url(self, mocker):
+        expected = {"mock_key": "mock_response"}
+        mocker.patch('aiohttp.ClientSession.get').return_value.__aenter__.return_value.json.return_value = expected
+        assert await get_as_json('mock_url') == expected
+
+
+class TestDmOpen:
+    @pytest.mark.asyncio
+    async def test_is_open(self, mocker, mock_discord_user):
+        mocker.patch('discord.HTTPException', MockException)
+        mock_discord_user.send.side_effect = MockException(code=50006)
+        assert await dm_open(mock_discord_user) is True
+
+    @pytest.mark.asyncio
+    async def test_is_closed(self, mocker, mock_discord_user):
+        mocker.patch('discord.HTTPException', MockException)
+        mock_discord_user.send.side_effect = MockException(code=50007)
+        assert await dm_open(mock_discord_user) is False
+
+    @pytest.mark.asyncio
+    async def test_is_neither(self, mocker, mock_discord_user):
+        mocker.patch('discord.HTTPException', MockException)
+        mock_discord_user.send.side_effect = MockException(code=50008)
+        with pytest.raises(MockException):
+            await dm_open(mock_discord_user)
